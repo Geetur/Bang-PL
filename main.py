4cfa572bd1a068e64f4771754d90fe51d43bf90b
Jeter Pontes <jeter.pontes@outlook.com>
2025-06-29 13:29:32 -0700

    the interpreter is up and running. solivng simple algorithms, but more complete testing required

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from lexer import Lexer
 from expression_parser import ExpressionParser, ParserError
 from control_flow_parser import ControlFlowParser
 from semantic_analysis import SemanticAnalysis, SemanticError
+from evaluator import Evaluator, EvaluatorError
 
 def main():
     # 1) Lex & tokenize
-    lexer = Lexer("input.txt")
+    lexer = Lexer("input2.txt")
     tokens = lexer.tokenizer()
     file = lexer.file                # source‐file lines for error reporting
     
@@ -16,8 +17,6 @@ def main():
     try:
         # 3) Build flat expression/control‐keywords stream
         all_expr_nodes = expr_parser.loading_into_algos()
-        print(all_expr_nodes)
-        
         
         # 4) Nest by control flow
         cf_parser = ControlFlowParser(file, all_expr_nodes)  
@@ -28,16 +27,12 @@ def main():
         sem = SemanticAnalysis(file, block_nodes)
         sem.walk_program()
 
-
         # 5) Print out the fully nested AST
-        for idx, node in enumerate(block_nodes, start=1):
-            print(f"Node {idx}: {node!r}")
+        ev  = Evaluator(file, block_nodes)
+        ev.eval_program()
 
-    except ParserError as pe:
-        # nicely print parsing errors with location
-        print(pe)
-    except Exception as e:
-        print(f"Unexpected error during parsing: {e}")
+    except (ParserError, SemanticError, EvaluatorError) as err:
+        print(err)
 
 if __name__ == "__main__":
     main()
